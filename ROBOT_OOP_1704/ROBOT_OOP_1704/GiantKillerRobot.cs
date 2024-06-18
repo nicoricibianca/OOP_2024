3a5c139d28b7a53ec02d43c7c05426ef1fd6ec3a
nicoricibianca <147925203+nicoricibianca@users.noreply.github.com>
2024-06-18 05:53:18 +0300

    Add files via upload

---
+++ ROBOT_OOP_1704/ROBOT_OOP_1704/GiantKillerRobot.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace RobotAttack
+{
+    enum Intensity
+    {
+        Stun, Kill, Destroy
+    }
+    enum TargetType
+    {
+        Animals, Humans, Superheroes
+    }
+    class Target
+    {
+        public TargetType Type { get; set; }
+        public bool IsAlive { get; set; } = true;
+
+        public Target(TargetType type)
+        {
+            Type = type;
+        }
+    }
+
+    class Planet
+    {
+        public bool ContainsLife { get; set; } = true;
+    }
+
+    internal class GiantKillerRobot
+    {
+
+        public Intensity EyeLaserIntensity { get; set; }
+        public List<Target> Targets { get; set; }
+        public bool Active { get; set; }
+        private int currentTargetIndex;
+
+        public Target CurrentTarget => Targets[currentTargetIndex];
+
+        public void Initialize()
+        {
+            Active = true;
+            currentTargetIndex = 0;
+        }
+        public void FireLaserAt(Target target)
+        {
+            if (EyeLaserIntensity == Intensity.Kill)
+            {
+                target.IsAlive = false;
+                Console.WriteLine($"Fired at {target.Type} - Target is now dead.");
+            }
+        }
+        public void AcquireNextTarget()
+        {
+            currentTargetIndex++;
+            if (currentTargetIndex >= Targets.Count)
+            {
+                Active = false;
+                Console.WriteLine("No more targets left. Robot is now inactive.");
+            }
+        }
+    }
+}
---
+++ ROBOT_OOP_1704/ROBOT_OOP_1704/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RobotAttack;
+
+GiantKillerRobot robot = new GiantKillerRobot();
+robot.Initialize();
+
+robot.EyeLaserIntensity = Intensity.Kill;
+
+robot.Targets = new List<Target>
+ {
+    new Target(TargetType.Animals),
+    new Target(TargetType.Humans),
+    new Target(TargetType.Superheroes)
+ };
+
+Planet earth = new Planet();
+
+while (robot.Active && earth.ContainsLife)
+{
+    if (robot.CurrentTarget.IsAlive)
+    {
+        robot.FireLaserAt(robot.CurrentTarget);
+    }
+    else
+    {
+        robot.AcquireNextTarget();
+    }
+}
+
